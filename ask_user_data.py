673920e767a9e02a0f019cadfb053bd8d852cc65
noob-wale <52056207+noob-wale@users.noreply.github.com>
2019-07-09 20:20:42 +0300

    Add files via upload

---
+++ ask_user_data.py
@@ -0,0 +1,4 @@
+user_name = input('Enter your name: ')
+print('Hello, {}, I am Lisa.'.format(user_name))
+user_country = input('Which country do you live in? ')
+print('How is {}?'.format(user_country))
